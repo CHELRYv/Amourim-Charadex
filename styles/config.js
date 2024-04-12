51e3c35e424f342a821467c7f0c03c22a7f81041
chel <89554079+CHELRYv@users.noreply.github.com>
2024-04-12 19:02:35 -0400

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1VZ7hWhNzFbJuX3pTgR6MNoA1_WRNrqXLGorbt2_o350";
 
 
 /* ------------------------------------------------------------------- */
